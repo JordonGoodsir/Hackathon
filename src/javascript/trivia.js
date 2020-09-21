741aab0130797c2530436435e83d7adf85459138
Ashley Smith <ash.mash.134@gmail.com>
2020-09-21 10:50:48 +1000

    Set up button functionality

--- src/javascript/trivia.js
+++ src/javascript/trivia.js
@@ -18,21 +18,26 @@ function decodeHtml(str){
 
 const questions = document.getElementById('questions')
 
-fetchTrivaJSON().then(trivia => {
-  console.log(trivia.results)
-  trivia.results.map(question =>  {
-    let thisQuestion = document.createElement('p')
-    thisQuestion.textContent = `${decodeHtml(question.question)}`
-    let ul = document.createElement('ul')
-    let correctAnswer = document.createElement('li')
-    correctAnswer.textContent = `Correct Answer: ${decodeHtml(question.correct_answer)}`
-    thisQuestion.appendChild(ul).appendChild(correctAnswer)
-    for (answer in question.incorrect_answers) {
-      let li = document.createElement('li')
-      li.textContent = decodeHtml(question.incorrect_answers[answer])
-      ul.appendChild(li)
-    }
+function getQuestions() {
+  questions.innerHTML = ""
+  fetchTrivaJSON().then(trivia => {
+    console.log(trivia.results)
+    trivia.results.map(question =>  {
+      let thisQuestion = document.createElement('p')
+      thisQuestion.textContent = `${decodeHtml(question.question)}`
+      let ul = document.createElement('ul')
+      let correctAnswer = document.createElement('li')
+      correctAnswer.textContent = `Correct Answer: ${decodeHtml(question.correct_answer)}`
+      thisQuestion.appendChild(ul).appendChild(correctAnswer)
+      for (answer in question.incorrect_answers) {
+        let li = document.createElement('li')
+        li.textContent = decodeHtml(question.incorrect_answers[answer])
+        ul.appendChild(li)
+      }
 
-    questions.appendChild(thisQuestion)
+      questions.appendChild(thisQuestion)
+    })
   })
-})
+}
+
+document.getElementById("button").addEventListener("click", getQuestions)
