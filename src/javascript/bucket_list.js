0ae17d79e4282d5ff1a6cba4ea21feb64a1810a0
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-09-21 14:39:37 +1000

    activity api button generate working

---
+++ src/javascript/bucket_list.js
@@ -0,0 +1,47 @@
+"https://www.boredapi.com/api/activity/"
+
+
+let fetchBucketList = async () => {   
+
+document.getElementById("list").innerHTML = ""
+
+let activities = [] 
+    for(let i=0; i < 10; i++){ 
+    
+    await fetch("https://www.boredapi.com/api/activity/") 
+    .then(response => response.json())  
+    .then(data => activities.push(data))
+   } 
+   
+document.getElementById("list").innerHTML = ""
+
+  
+   activities.forEach((v) => {  
+
+    let li = document.createElement("LI")  
+
+    let liText = document.createTextNode(v.activity) 
+    
+    li.appendChild(liText)
+
+    document.getElementById("list").appendChild(li)
+ 
+   })
+
+
+   
+    // const response = await fetch("https://www.boredapi.com/api/activity/")
+    // const list = await response.json() 
+
+    
+    // // look into promise
+    
+    // for(i = 0; i == 10; i++) { 
+    // await list  
+    // console.log(list)
+    // }
+}   
+
+let generate = document.getElementById("generate");
+
+generate.addEventListener("click", fetchBucketList)
