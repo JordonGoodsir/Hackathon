d2d512ccd57a5da8c611c445e2766b065d67a514
Ashley Smith <ash.mash.134@gmail.com>
2020-09-21 13:18:22 +1000

    Set up form to get specific api

--- src/javascript/trivia.js
+++ src/javascript/trivia.js
@@ -1,34 +1,42 @@
-async function fetchTrivaJSON() {
-  const response = await fetch('https://opentdb.com/api.php?amount=10')
+async function fetchTrivaJSON(number, category, difficulty, type) {
+  let api = `https://opentdb.com/api.php?amount=${number}&category=${category}&difficulty=${difficulty}&type=${type}`
+  if (category === 'any') { api = api.replace(`&category=${category}`, '')}
+  if (difficulty === 'any') { api = api.replace(`&difficulty=${difficulty}`, '') }
+  if (type === 'any') { api = api.replace(`&type=${type}`, '') }  
+  const response = await fetch(api)
   const trivia = await response.json()
   return trivia
 }
 
-function decodeHtml(str){
-  let map =
-    {
-        '&amp;': '&',
-        '&lt;': '<',
-        '&gt;': '>',
-        '&quot;': '"',
-        '&#039;': "'"
-    };
-    return str.replace(/&amp;|&lt;|&gt;|&quot;|&#039;/g, function(m) {return map[m];});
+function decodeHtml(html) {
+  var txt = document.createElement("textarea");
+  txt.innerHTML = html;
+  return txt.value;
 }
 
 const questions = document.getElementById('questions')
 
 function getQuestions() {
+  let formEl = document.forms.formdata; 
+  let formData = new FormData(formEl);
+  let questionNumber = formData.get('trivia_amount')
+  let category = formData.get('trivia_category')
+  let difficulty = formData.get('trivia_difficulty')
+  let type = formData.get('trivia_type')
+  console.log(questionNumber, category, difficulty, type)
+
   questions.innerHTML = ""
-  fetchTrivaJSON().then(trivia => {
-    console.log(trivia.results)
+  fetchTrivaJSON(questionNumber, category, difficulty, type).then(trivia => {
     trivia.results.map(question =>  {
       let thisQuestion = document.createElement('p')
       thisQuestion.textContent = `${decodeHtml(question.question)}`
+
       let ul = document.createElement('ul')
       let correctAnswer = document.createElement('li')
+
       correctAnswer.textContent = `Correct Answer: ${decodeHtml(question.correct_answer)}`
       thisQuestion.appendChild(ul).appendChild(correctAnswer)
+
       for (answer in question.incorrect_answers) {
         let li = document.createElement('li')
         li.textContent = decodeHtml(question.incorrect_answers[answer])
@@ -40,4 +48,4 @@ function getQuestions() {
   })
 }
 
-document.getElementById("button").addEventListener("click", getQuestions)
+// document.getElementById('button').addEventListener('click', getQuestions)
