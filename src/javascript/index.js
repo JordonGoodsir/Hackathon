ad7c7627ecc63230b7d9c983e6a21cea1385332a
Ashley Smith <ash.mash.134@gmail.com>
2020-09-22 10:11:34 +1000

    Added comments for cat image

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,20 +1,25 @@
+// Sets up for modal use when cat button is clicked
 let btn = document.getElementById('cat')
 let modalPic = document.getElementById('modal_pic')
 let modal = document.getElementById('myModal')
 let span = document.getElementsByClassName('close')[0]
 let catAudio = new Audio('audio/meow.wav')
 
+// Returns a random number to display cat. Currently there are 16 random cat images available
 function getRandomNumberForKittenAPI() {
   let suitableNumber = Math.floor(Math.random() * 15) + 1;
   return suitableNumber;
 }
 
+// Displays cat and meows
 function displayKitten() {
-  catAudio.currentTime = 0;
-  
+  // Sets audio to 0 time so it will restart on click of button
+  catAudio.currentTime = 0;  
   catAudio.play()
 
+  // Removes old image
   modalPic.innerHTML = ''
+
   const catPic = document.getElementById('cat-pic')  
   let catImage = document.createElement('img')
   catImage.src = `https://placekitten.com/200/300?image=${getRandomNumberForKittenAPI()}`
@@ -37,6 +42,7 @@ window.onclick = function(event) {
   }
 }
 
+// Converts text into html friendly text
 function decodeHtml(html) {
   let txt = document.createElement("textarea");
   txt.innerHTML = html;
--- src/javascript/trivia.js
+++ src/javascript/trivia.js
@@ -1,13 +1,19 @@
+// Gets the details from the trivia api
 async function fetchTrivaJSON(number, category, difficulty, type) {
+  // This is the format the trivia api is in when all categories are selected
   let api = `https://opentdb.com/api.php?amount=${number}&category=${category}&difficulty=${difficulty}&type=${type}`
+  
+  // If any selection is any it is removed from the above api
   if (category === 'any') { api = api.replace(`&category=${category}`, '')}
   if (difficulty === 'any') { api = api.replace(`&difficulty=${difficulty}`, '') }
   if (type === 'any') { api = api.replace(`&type=${type}`, '') }  
+  
   const response = await fetch(api)
   const trivia = await response.json()
   return trivia
 }
 
+// Converts text into html friendly text
 function decodeHtml(html) {
   let txt = document.createElement("textarea");
   txt.innerHTML = html;
@@ -16,7 +22,9 @@ function decodeHtml(html) {
 
 const questions = document.getElementById('questions')
 
+// Generates the list of questions and displays them to the screen
 function getQuestions() {
+  // Gets data from the form and allocates it to the right area
   let formEl = document.forms.formdata; 
   let formData = new FormData(formEl);
   let questionNumber = formData.get('trivia_amount')
@@ -24,22 +32,29 @@ function getQuestions() {
   let difficulty = formData.get('trivia_difficulty')
   let type = formData.get('trivia_type')
 
+  // Wipes previously loaded data
   questions.innerHTML = ""
 
+  // Uses the api to generate the question list
   fetchTrivaJSON(questionNumber, category, difficulty, type).then(trivia => {
     trivia.results.map(question =>  {
+
+      // Creates element for questions to be added to
       let thisQuestion = document.createElement('p')
       thisQuestion.textContent = `${decodeHtml(question.question)}`
       thisQuestion.style.fontWeight = '800'
 
+      // Creates list for display purposes
       let ul = document.createElement('ul')
       let correctAnswer = document.createElement('li')
       ul.style.fontWeight = '400'
 
+      // Shows the correct answer
       correctAnswer.textContent = `Correct Answer: ${decodeHtml(question.correct_answer)}`
       thisQuestion.appendChild(ul).appendChild(correctAnswer)
       correctAnswer.style.fontWeight = '500'
 
+      // Loops through all incorret options and displays them
       for (answer in question.incorrect_answers) {
         let li = document.createElement('li')
         li.textContent = decodeHtml(question.incorrect_answers[answer])
@@ -51,5 +66,6 @@ function getQuestions() {
   })
 }
 
+const triviaButton = document.getElementById('trivia-button')
 
-
+triviaButton.addEventListener('click', getQuestions)
