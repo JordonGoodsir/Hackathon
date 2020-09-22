1ba6ba9279fc82a77875f449b7965945a3209336
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-09-22 12:23:03 +1000

    bucket list styling and social distancing option implemented

--- src/javascript/bucket_list.js
+++ src/javascript/bucket_list.js
@@ -1,45 +1,61 @@
-"https://www.boredapi.com/api/activity/"
+"https://www.boredapi.com/api/activity/" 
+
+activityType = { 
+    social: "fa-users", 
+    charity:"fa-hand-holding-heart", 
+    busywork:"fa-briefcase", 
+    recreational:"fa-laugh-beam", 
+    education:"fa-book-open",
+    relaxation:"fa-peace",
+    music:"fa-music", 
+    diy: "fa-hammer", 
+    cooking:"fa-cookie-bite"
+}
+
 
 
 let fetchBucketList = async () => {   
 
-document.getElementById("list").innerHTML = ""
+document.getElementById("list").innerHTML = "" 
+
+console.log(document.getElementById("social_distance").checked)
 
 let activities = [] 
     for(let i=0; i < 10; i++){ 
     
     await fetch("https://www.boredapi.com/api/activity/") 
     .then(response => response.json())  
-    .then(data => activities.push(data))
-   } 
+    .then(data => { 
+    if (document.getElementById("social_distance").checked) {
+      data.type != "social" ? activities.push(data) : i--
+    } else { 
+       activities.push(data) 
+    }
+
+   }) 
+}
    
 document.getElementById("list").innerHTML = ""
 
-  
    activities.forEach((v) => {  
 
     let li = document.createElement("LI")  
 
     let liText = document.createTextNode(v.activity) 
+
+    let i = document.createElement("I")  
+
+    i.classList.add("fas")   
+
+    i.classList.add(`${activityType[v.type]}`)  
+
+    li.appendChild(i)
     
     li.appendChild(liText)
 
-    document.getElementById("list").appendChild(li)
- 
+    document.getElementById("list").appendChild(li)  
+
    })
-
-
-   
-    // const response = await fetch("https://www.boredapi.com/api/activity/")
-    // const list = await response.json() 
-
-    
-    // // look into promise
-    
-    // for(i = 0; i == 10; i++) { 
-    // await list  
-    // console.log(list)
-    // }
 }   
 
 let generate = document.getElementById("generate");
