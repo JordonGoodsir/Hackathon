c025d73e5006be92f506681798ef768850a1aa1a
Ashley Smith <ash.mash.134@gmail.com>
2020-09-22 11:54:33 +1000

    finished styling cocktails

--- src/javascript/cocktail.js
+++ src/javascript/cocktail.js
@@ -1,11 +1,11 @@
-// window.onload = function() {
-//   if (confirm('Please select ok if you are over legal drinking age.')) {
-//   // Save it!
-//   } else {
-//     // Do nothing!
-//     alert(`Sorry you're too young to be here`)
-//     window.open('index.html', '_self')};
-// }
+window.onload = function() {
+  if (confirm('Please select ok if you are over legal drinking age.')) {
+  // Save it!
+  } else {
+    // Do nothing!
+    alert(`Sorry you're too young to be here`)
+    window.open('index.html', '_self')};
+}
 
 async function fetchCocktailJSON() {
   const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
@@ -33,21 +33,21 @@ function getCocktail() {
 
     for (item in thisDrink) {
       if (item.includes('strGlass')) {
-        let glass = document.createElement('li')
+        let glass = document.createElement('p')
         glass.textContent = `Recommended glass: ${thisDrink[item]}`
         cocktailDiv.appendChild(glass)
       }
       if (item.includes('strIngredient') && thisDrink[item] !== null && thisDrink[item]  !== '') {
         let strMeasure = `strMeasure${item.slice(-1)}`
         if (!isNaN(item.slice(-2))) { strMeasure = `strMeasure${item.slice(-2)}` }
-        let ingredient = document.createElement('li')
+        let ingredient = document.createElement('p')
         ingredient.textContent = (thisDrink[strMeasure] === null) ?`Ingredient: ${thisDrink[item]}` : `Ingredient: ${thisDrink[item]} - measure ${thisDrink[strMeasure]}`
 
         cocktailDiv.appendChild(ingredient)
       }
     }
 
-    let instructions = document.createElement('li')
+    let instructions = document.createElement('p')
     instructions.textContent = `Instructions: ${thisDrink.strInstructions}`
     cocktailDiv.appendChild(instructions)
   })
